70e9cc56340afe82d6179d71e0f9b32344c62759
deds129 <nik.chudinov129@mail.ru>
2023-01-25 14:53:02 +0300

    change error displaying

--- src/main/java/sia/tacocloud/tacos/model/TacoOrder.java
+++ src/main/java/sia/tacocloud/tacos/model/TacoOrder.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
-import org.hibernate.validator.constraints.CreditCardNumber;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -34,7 +33,7 @@ public class TacoOrder {
 	@NotBlank(message="Zip code is required")
 	private String deliveryZip;
 	
-	@CreditCardNumber(message="Not a valid credit card number")
+	//@CreditCardNumber(message="Not a valid credit card number")
 	private String ccNumber;
 	
 	@Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$",
