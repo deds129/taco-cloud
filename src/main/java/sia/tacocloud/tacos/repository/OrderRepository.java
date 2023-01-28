40ec39c79c93bddb925d4c09101f174d9427b5da
deds129 <nik.chudinov129@mail.ru>
2023-01-28 23:40:48 +0300

    Spring Data JPA

--- src/main/java/sia/tacocloud/tacos/repository/OrderRepository.java
+++ src/main/java/sia/tacocloud/tacos/repository/OrderRepository.java
@@ -3,6 +3,11 @@ package sia.tacocloud.tacos.repository;
 import org.springframework.data.repository.CrudRepository;
 import sia.tacocloud.tacos.model.TacoOrder;
 
+import java.util.List;
+
 public interface OrderRepository
 		extends CrudRepository<TacoOrder, Long> {
+
+	List<TacoOrder> findByDeliveryZip(String deliveryZip);
+
 }
